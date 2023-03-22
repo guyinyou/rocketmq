96437a92c07fad071950ffd300e7cb5ee37ea2f7
guyinyou <guyinyou.gyy@alibaba-inc.com>
2023-03-22 11:54:28 +0800

    scan topic.json to find compactionTopic and copy it

--- store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
+++ store/src/main/java/org/apache/rocketmq/store/DefaultMessageStore.java
@@ -2032,6 +2032,11 @@ public class DefaultMessageStore implements MessageStore {
     }
 
     @Override
+    public ConcurrentMap<String, TopicConfig> getTopicConfigs() {
+        return this.consumeQueueStore.getTopicConfigs();
+    }
+
+    @Override
     public Optional<TopicConfig> getTopicConfig(String topic) {
         return this.consumeQueueStore.getTopicConfig(topic);
     }
